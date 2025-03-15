0357048eae0f1e48fd91070aa56f92567ef179cd
marianachoratto <marianachoratto@gmail.com>
2025-03-15 19:32:27 -0300

    add imagens habilidades

--- src/components/Habilidades.tsx
+++ src/components/Habilidades.tsx
@@ -1,9 +1,25 @@
+import styled from "styled-components";
+import flag_eua from "../assets/flag_eua.png";
 import { useChangeLanguageFunction } from "../utils/changeLanguageFunction";
+import { listTitles } from "../utils/ListOfHabilities";
 
 export function Habilidades() {
+  const titulos = listTitles.map((titulo) => {
+    return <h1>{titulo}</h1>;
+  });
   return (
     <>
       <h1>{useChangeLanguageFunction("habilidades")}</h1>
+
+      <ContainerHabilidades className="d-flex flex-wrap row-gap-5 column-gap-4 column-gap-sm-5 justify-content-center column-gap-lg-5 w-100">
+        <div className="d-flex flex-column-aic gap-2 col-4 col-sm-5 col-lg-3 hidden">
+          {titulos}
+
+          <img src={flag_eua} alt="" />
+        </div>
+      </ContainerHabilidades>
     </>
   );
 }
+
+const ContainerHabilidades = styled.div``;
---
+++ src/utils/ListOfHabilities.ts
@@ -0,0 +1,14 @@
+export const listTitles = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Typescript",
+  "Python",
+  "Git",
+  "Github",
+  "React",
+  "Bootstrap",
+  "Django",
+  "Django Rest Framework",
+  "NodeJS",
+];
