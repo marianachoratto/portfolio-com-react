193f14d21fb45e0264cd4cf9d79623e654dc3297
marianachoratto <marianachoratto@gmail.com>
2025-03-30 18:36:55 -0300

    arrumando troca de idioma dos projetos

--- src/components/Projetos.tsx
+++ src/components/Projetos.tsx
@@ -4,7 +4,10 @@ import {
   DivProjeto,
   TituloHabilidade,
 } from "./ProjetosStyled";
-import { useChangeLanguageFunction } from "../utils/changeLanguageFunction";
+import {
+  changeLanguageTitle,
+  useChangeLanguageFunction,
+} from "../utils/changeLanguageFunction";
 import { FiExternalLink } from "react-icons/fi";
 import { FaGithub } from "react-icons/fa";
 import { useState } from "react";
@@ -18,47 +21,52 @@ export function Projetos() {
     <div>
       <h1 className="mb-5">{useChangeLanguageFunction("projetosTitulo")}</h1>
       <CardsProjetoPai>
-        {listaProjetos.map((card) => (
-          <DivProjeto>
-            <h2 className="mb-3">{card.tituloProjeto}</h2>
-            <a href={card.linkAcesse}>
-              <img src={card.imagem} />
-            </a>
-            <div className="d-flex flex-wrap gap-2 row-gap-3 w-100 justify-content-center mb-2 mt-3 ">
-              {card.skills.map((skill) => (
-                <TituloHabilidade>{skill}</TituloHabilidade>
-              ))}
-            </div>
-            <p>
-              {card.descricaoBr.split(" ").slice(0, 15).join(" ")}
-              {"..."}
-            </p>
-            <div className="d-flex justify-content-center gap-3">
-              <a>
-                <Button
-                  type="button"
-                  className="btn btn-light"
-                  onClick={() => {
-                    console.log("oi", buttonVerMais);
-                    return setbuttonVerMais(!buttonVerMais);
-                  }}
-                >
-                  {useChangeLanguageFunction("verMais")}
-                </Button>
-              </a>
+        {listaProjetos.map((card, index) => {
+          const descricaoProjeto = changeLanguageTitle(index, "descricao");
+
+          return (
+            <DivProjeto>
+              <h2 className="mb-3">{changeLanguageTitle(index, "titulo")}</h2>
               <a href={card.linkAcesse}>
-                <Button type="button" className="btn btn-light">
-                  {useChangeLanguageFunction("acesse")} <FiExternalLink />
-                </Button>
+                <img src={card.imagem} />
               </a>
-              <a href={card.linkGithub}>
-                <Button type="button" className="btn btn-light ">
-                  <FaGithub size={30} />
-                </Button>
-              </a>
-            </div>
-          </DivProjeto>
-        ))}
+              <div className="d-flex flex-wrap gap-2 row-gap-3 w-100 justify-content-center mb-2 mt-3 ">
+                {card.skills.map((skill) => (
+                  <TituloHabilidade>{skill}</TituloHabilidade>
+                ))}
+              </div>
+              <p>
+                {descricaoProjeto.split(" ").slice(0, 15).join(" ")}
+
+                {"..."}
+              </p>
+              <div className="d-flex justify-content-center gap-3">
+                <a>
+                  <Button
+                    type="button"
+                    className="btn btn-light"
+                    onClick={() => {
+                      console.log("oi", buttonVerMais);
+                      return setbuttonVerMais(!buttonVerMais);
+                    }}
+                  >
+                    {useChangeLanguageFunction("verMais")}
+                  </Button>
+                </a>
+                <a href={card.linkAcesse}>
+                  <Button type="button" className="btn btn-light">
+                    {useChangeLanguageFunction("acesse")} <FiExternalLink />
+                  </Button>
+                </a>
+                <a href={card.linkGithub}>
+                  <Button type="button" className="btn btn-light ">
+                    <FaGithub size={30} />
+                  </Button>
+                </a>
+              </div>
+            </DivProjeto>
+          );
+        })}
       </CardsProjetoPai>
       {/* Modal  */}
       {buttonVerMais && <ModalProjeto />}
--- src/utils/changeLanguageFunction.ts
+++ src/utils/changeLanguageFunction.ts
@@ -1,8 +1,15 @@
 import { useGlobalContext } from "../App";
+import { listaProjetos } from "./listaProjetos";
 import { objectLanguage } from "./objectLanguage";
 
 export function useChangeLanguageFunction(title: string) {
   const lingua = useGlobalContext();
 
   return objectLanguage[lingua.language][title];
+}
+
+export function changeLanguageTitle(index: number, title: string) {
+  const lingua = useGlobalContext();
+
+  return listaProjetos[index][`${title}${lingua.language}`];
 }
